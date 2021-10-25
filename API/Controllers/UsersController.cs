f917781a8f7874b6e8ecbc31d8e6b486c420e584
davidax0204 <71772975+davidax0204@users.noreply.github.com>
2021-10-25 19:38:51 +0300

    DTO, authentication, JWT

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,12 +6,11 @@ using API.Entities;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         public DataContext _context { get; }
         public UsersController(DataContext context)
@@ -19,12 +18,14 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        [Authorize]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
---
+++ API/Expensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Expensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
